360ba8164758a0c732bdd1cc2a535c2e6ad1baea
Adam Smith <apsmith@ccs.neu.edu>
2019-02-24 18:12:49 -0500

    added descriptions

--- hw09_APS.rkt
+++ hw09_APS.rkt
@@ -1,10 +1,13 @@
 #lang pl 09
 
+;; Y-combinator takes a single-argument function and returns a recursive
+;; version
 (define (Y f)
   ((lambda (x) (x x))
    (lambda (x) (f (lambda (z) ((x x) z))))))
 
-
+;; define/rec takes a multi-argument function and returns a recursive version
+;; by applying the Y-combinator
 (rewrite (define/rec (f x ...) E)
          => (define f
               (let ([g (Y (lambda (f)
@@ -14,14 +17,20 @@
                                   E)))))])
                 (g #f))))
 
+;; the Ackermann function is one of the simplest and earliest-discovered
+;; examples of a total computable function that is not primitive recursive.
+;; source: https://en.wikipedia.org/wiki/Ackermann_function
 (define/rec (ackermann m n)
   (cond [(zero? m) (+ n 1)]
         [(zero? n) (ackermann (- m 1) 1)]
         [else      (ackermann (- m 1) (ackermann m (- n 1)))]))
 
+;; tests for ackermann
 (test (ackermann 3 3) => 61)
 (test (ackermann 4 0) => 13)
 
+;; letfuns takes a specially formed mutually referrential set of functions
+;; and returns the recurisive version
 (rewrite (letfuns ([(f x ...) E] ...) B)
          => (let ([g (Y (lambda (funs)
                           (lambda (name)
@@ -32,6 +41,7 @@
               (let ([f (g 'f)] ...)
                 B)))
 
+;; tests for even?/odd?
 (test (not (letfuns ([(even? n) (or (= n 0) (odd?  (- n 1)))]
                      [(odd?  n) (if (= n 0) #f (even? (- n 1)))])
                     (even? 123))))
@@ -42,6 +52,9 @@
       => (list #f #t #t #f))
 
 ;; an extended example
+;; scan is an automaton (a state machine) that consumes a string,
+;; and returns #t if the parens in the string are balanced, and if every
+;; continuous block of numeric characters is strictly increasing.
 (define scan
   (letfuns ([(start str)  (loop (explode-string str) 0)]
             [(loop l n)   (match l
@@ -60,6 +73,8 @@
                              (and (< m m1) (nums more m1 n))]
                             [else (loop l n)])])
            start))
+
+;; tests for scan
 (test (scan "(()123(246x12)) (blah)"))
 (test (not (scan "(1232)")))
 (test (not (scan "()(")))
